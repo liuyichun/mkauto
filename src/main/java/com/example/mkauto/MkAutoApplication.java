97766918a45cd8c10a744d82dc6cea43d93933ca
boren <liuyc@adayun.com>
2022-01-10 16:50:05 +0800

    add

---
+++ src/main/java/com/example/mkauto/MkAutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mkauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkAutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mkauto/MkAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mkauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
